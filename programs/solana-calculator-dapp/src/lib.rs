ca943a18e1832ffd1f7ce9da4c1395baec67772e
Awesome <dev>
2022-04-15 12:32:53 +0530

    feat(create): create new extrinsic create

--- programs/solana-calculator-dapp/src/lib.rs
+++ programs/solana-calculator-dapp/src/lib.rs
@@ -3,13 +3,14 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod solana_calculator_dapp {
+pub mod mycalculatorapp {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, init_messge: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_messge;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
